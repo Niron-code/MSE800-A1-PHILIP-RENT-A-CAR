e2a10953123416143863701edc97da2bea8db727
Philip Niron Nithianandan <philip.niron@gmail.com>
2025-09-03 14:18:48 +1200

    Update user menu call to use UserController class
    Replaces the function call to user_main_menu with a class method call UserController.user_main_menu for improved structure and consistency.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from controllers.user_controller import user_main_menu
+from controllers.user_controller import UserController
 from database import init_db
 
 init_db()
 
 def main():
-    user_main_menu()
+    UserController.user_main_menu()
 
 if __name__ == "__main__":
     main()
- 
