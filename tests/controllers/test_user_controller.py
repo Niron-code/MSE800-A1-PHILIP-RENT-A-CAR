512594f0a62e85104ca9be985cf6a96edddd2e05
Philip Niron Nithianandan <philip.niron@gmail.com>
2025-09-16 22:13:14 +1200

    MSE800A1 - Refactor tests to use Utils.load_texts for text loading
    Updated test cases in test_user_controller.py to use Utils.load_texts('en') for loading text resources instead of accessing user_controller.txts directly. This improves consistency and decouples tests from internal controller variables.

--- tests/controllers/test_user_controller.py
+++ tests/controllers/test_user_controller.py
@@ -4,30 +4,34 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 import pytest
 from controllers.user_controller import UserController
 from controllers import user_controller
+from utils import Utils
 
 class TestUserController:
     def test_main_menu_texts(self, monkeypatch, capsys):
+        texts = Utils.load_texts('en')
         # Simulate user entering '0' to exit
         monkeypatch.setattr('builtins.input', lambda _: '0')
         with pytest.raises(SystemExit):
-            UserController.user_main_menu(user_controller.txts)
+            UserController.user_main_menu(texts)
         captured = capsys.readouterr()
-        assert user_controller.txts['txt_welcome'] in captured.out
-        assert user_controller.txts['txt_are_you_admin_or_customer'] in captured.out
-        assert user_controller.txts['txt_admin_option'] in captured.out
-        assert user_controller.txts['txt_customer_option'] in captured.out
-        assert user_controller.txts['txt_exit_option'] in captured.out
+        assert texts['txt_welcome'] in captured.out
+        assert texts['txt_are_you_admin_or_customer'] in captured.out
+        assert texts['txt_admin_option'] in captured.out
+        assert texts['txt_customer_option'] in captured.out
+        assert texts['txt_exit_option'] in captured.out
 
     def test_main_menu_invalid_choice(self, monkeypatch, capsys):
+        texts = Utils.load_texts('en')
         inputs = iter(['invalid', '0'])  # First invalid, then exit
         monkeypatch.setattr('builtins.input', lambda _: next(inputs))
         with pytest.raises(SystemExit):
-            UserController.user_main_menu(user_controller.txts)
+            UserController.user_main_menu(texts)
         captured = capsys.readouterr()
-        assert user_controller.txts['txt_invalid_choice'] in captured.out
-        assert user_controller.txts['txt_welcome'] in captured.out  # Menu displayed again
+        assert texts['txt_invalid_choice'] in captured.out
+        assert texts['txt_welcome'] in captured.out  # Menu displayed again
 
     def test_admin_login_success(self, monkeypatch, capsys):
+        texts = Utils.load_texts('en')
         # Simulate username and password input
         inputs = iter(['admin', 'secret'])
         monkeypatch.setattr('builtins.input', lambda _: next(inputs))
@@ -43,12 +47,13 @@ class TestUserController:
         # Patch admin_menu to exit after login
         monkeypatch.setattr(user_controller.AdminUserController, 'admin_menu', lambda username: exit())
         with pytest.raises(SystemExit):
-            user_controller.AdminUserController.admin_login(user_controller.txts)
+            user_controller.AdminUserController.admin_login(texts)
         captured = capsys.readouterr()
-        assert user_controller.txts['txt_admin_login'] in captured.out
-        assert user_controller.txts['txt_welcome_admin'].format(username='admin') in captured.out
+        assert texts['txt_admin_login'] in captured.out
+        assert texts['txt_welcome_admin'].format(username='admin') in captured.out
 
     def test_admin_login_fail(self, monkeypatch, capsys):
+        texts = Utils.load_texts('en')
         # Simulate username and password input
         inputs = iter(['admin', 'wrongpw'])
         monkeypatch.setattr('builtins.input', lambda _: next(inputs))
@@ -59,8 +64,8 @@ class TestUserController:
         with pytest.raises(SystemExit):
             # Patch user_main_menu to exit after failed login
             monkeypatch.setattr(user_controller.UserController, 'user_main_menu', lambda: exit())
-            user_controller.AdminUserController.admin_login(user_controller.txts)
+            user_controller.AdminUserController.admin_login(texts)
         captured = capsys.readouterr()
-        assert user_controller.txts['txt_admin_login'] in captured.out
-        assert user_controller.txts['txt_invalid_admin_credentials'] in captured.out
+        assert texts['txt_admin_login'] in captured.out
+        assert texts['txt_invalid_admin_credentials'] in captured.out
 
